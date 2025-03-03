8c8867c353ea2665bfc6695deb580e5289bbc3de
gustavoglins <gustavoglins05@gmail.com>
2025-03-03 01:11:05 -0300

    Initial commit

---
+++ src/main/java/com/festivo/FestivoApiApplication.java
@@ -0,0 +1,13 @@
+package com.festivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/festivo/FestivoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
